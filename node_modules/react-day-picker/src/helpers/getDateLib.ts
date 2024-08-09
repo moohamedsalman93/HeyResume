4cc71d5dbe65fd0fbf22d26015f0b714cf3d7be9
mohamedsalman <53552621+moohamedsalman93@users.noreply.github.com>
2024-08-09 18:00:02 +0530

    Delete node_modules directory

--- node_modules/react-day-picker/src/helpers/getDateLib.ts
+++
@@ -1,9 +0,0 @@
-import { dateLib } from "../lib/dateLib.js";
-import type { DateLib, DayPickerProps } from "../types/index.js";
-
-export function getDateLib(customLib: DayPickerProps["dateLib"]): DateLib {
-  return {
-    ...dateLib,
-    ...customLib
-  };
-}
--- node_modules/react-day-picker/src/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./dateLib.js";
